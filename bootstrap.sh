48b759c3682a17a7e67ffe9c59744b6c2d23eeba
nadyawilliams <nadya@sdsc.edu>
2017-12-21 15:19:15 -0800

    move sources on googledrive

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 # @Copyright@
 # @Copyright@
 #
-# set google url for dowloading sources
-#export SURL="https://drive.google.com/open?id="
+# set google url for downloading sources
+export SURL="https://drive.google.com/open?id=1GhZuRdqWzE2mZTJ87UEL0tay-Yh9oCP8"
 
 # download sources
 . /opt/rocks/share/devel/src/roll/etc/bootstrap-functions.sh
