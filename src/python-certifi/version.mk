0ca9dbeda575f63cf0661c817cca9b765a75091b
root <root@calit2-119-222.ucsd.edu>
2018-02-02 14:06:33 -0500

    update path for installed fiels

--- src/python-certifi/version.mk
+++ src/python-certifi/version.mk
@@ -8,6 +8,6 @@ TARBALL_POSTFIX = tar.gz
 RPM.EXTRAS = "AutoReq: no"
 
 RPM.FILES = \
-/opt/python/lib/python2.7/site-packages/certifi-2018.1.18* \n \
-/opt/python/lib/python2.7/site-packages/certifi/
+/opt/python/lib/python2.7/site-packages/certifi/ \n \
+/opt/python/lib/python2.7/site-packages/certifi-2018.1.18-py2.7.egg-info/
 
