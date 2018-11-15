3f55034e4743bcee7ed88729a99f54403f91f315
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-11-16 01:10:12 +0700

    refractor script

--- bae-ken/virtual-env/run-topology.sh
+++
@@ -1,9 +0,0 @@
-docker-compose -f ./influxdb/docker-compose.yml up -d && \
-docker-compose -f ./grafana/docker-compose.yml up -d && \
-docker-compose -f ./grafana-2/docker-compose.yml up -d && \
-docker-compose -f ./telegraf/docker-compose.yml up -d && \
-docker-compose -f ./kapacitor/docker-compose.yml up -d && \
-docker-compose -f ./kapacitor-2/docker-compose.yml up -d && \
-docker-compose -f ./chronograf/docker-compose.yml up -d && \
-docker-compose -f ./chronograf-2/docker-compose.yml up -d 
-
