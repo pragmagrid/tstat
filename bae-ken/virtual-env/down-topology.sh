76cff0abb93a44ea7e8d4e053d2caf6e874cc9fb
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-11-16 00:53:36 +0700

    down script fixed

--- bae-ken/virtual-env/down-topology.sh
+++ bae-ken/virtual-env/down-topology.sh
@@ -1,9 +1,9 @@
-docker-compose -f ./influxdb/docker-compose.yml down -d && \
-docker-compose -f ./grafana/docker-compose.yml down -d && \
-docker-compose -f ./grafana-2/docker-compose.yml down -d && \
-docker-compose -f ./telegraf/docker-compose.yml down -d && \
-docker-compose -f ./kapacitor/docker-compose.yml down -d && \
-docker-compose -f ./kapacitor-2/docker-compose.yml down -d && \
-docker-compose -f ./chronograf/docker-compose.yml down -d && \
-docker-compose -f ./chronograf-2/docker-compose.yml down -d 
+docker-compose -f ./influxdb/docker-compose.yml dow && \
+docker-compose -f ./grafana/docker-compose.yml down && \
+docker-compose -f ./grafana-2/docker-compose.yml down && \
+docker-compose -f ./telegraf/docker-compose.yml down && \
+docker-compose -f ./kapacitor/docker-compose.yml down && \
+docker-compose -f ./kapacitor-2/docker-compose.yml down && \
+docker-compose -f ./chronograf/docker-compose.yml down && \
+docker-compose -f ./chronograf-2/docker-compose.yml down 
 
