3b021de1bc8a4e1cceabddb4da4c1384bc037cf7
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-11-16 01:01:07 +0700

    script bug fixed

--- bae-ken/virtual-env/bin/run.clear.storage.sh
+++ bae-ken/virtual-env/bin/run.clear.storage.sh
@@ -3,5 +3,5 @@ rm -rf ./grafana/storage && \
 rm -rf ./grafana-2/storage && \
 rm -rf ./telegraf/storage && \
 rm -rf ./kapacitor/storage && \
-rm -rf ./kapacitor-2/storage && \
+rm -rf ./kapacitor-2/storage
 
